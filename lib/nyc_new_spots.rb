4fed3ae2a966866ca2f2a9e6519455a84bdacff4
hangloos <ericloos00@gmail.com>
2016-05-20 16:58:51 -0400

    Error 503

--- lib/nyc_new_spots.rb
+++ lib/nyc_new_spots.rb
@@ -1,10 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require "nyc_new_spots/version"
 require 'nyc_new_spots/cli'
 require 'nyc_new_spots/spots'
 
-require 'open-uri'
-require 'pry'
-require 'nokogiri'
 
-module NycNewSpots
-end
--- lib/nyc_new_spots/spots.rb
+++ lib/nyc_new_spots/spots.rb
@@ -2,38 +2,41 @@ class NycNewSpots::Spots
 
 attr_accessor :name, :address,:location, :categories, :price_range, :hours, :more_business_information, :phone_number
 
-def self.today
-spot_1 = self.new
-spot_1.name = "Ludlow Coffee Supply"
-spot_1.location = "Lower East Side"
-spot_1.address = "176 Ludlow St New York, NY 10002"
-spot_1.categories = "Coffee & Tea, Breakfast & Brunch"
-spot_1.price_range = "Under $10"
-spot_1.hours = "Mon 8:00am - 8:00pm"
-spot_1.phone_number = "(212) 777-7465"
-spot_1.more_business_information = "Test More Business Info!"
+  def self.today
+    spot_1 = self.new
+    spot_1.name = "Ludlow Coffee Supply"
+    spot_1.location = "Lower East Side"
+    spot_1.address = "176 Ludlow St New York, NY 10002"
+    spot_1.categories = "Coffee & Tea, Breakfast & Brunch"
+    spot_1.price_range = "Under $10"
+    spot_1.hours = "Mon 8:00am - 8:00pm"
+    spot_1.phone_number = "(212) 777-7465"
+    spot_1.more_business_information = "Test More Business Info!"
 
-spot_2 = self.new
-spot_2.name = "Ludlow Coffee Supply"
-spot_2.location = "Lower East Side"
-spot_2.address = "176 Ludlow St New York, NY 10002"
-spot_2.categories = "Coffee & Tea, Breakfast & Brunch"
-spot_2.price_range = "Under $10"
-spot_2.hours = "Mon 8:00am - 8:00pm"
-spot_2.phone_number = "(212) 777-7465"
-spot_2.more_business_information = "Test More Business Info!"
+    spot_2 = self.new
+    spot_2.name = "Ludlow Coffee Supply"
+    spot_2.location = "Lower East Side"
+    spot_2.address = "176 Ludlow St New York, NY 10002"
+    spot_2.categories = "Coffee & Tea, Breakfast & Brunch"
+    spot_2.price_range = "Under $10"
+    spot_2.hours = "Mon 8:00am - 8:00pm"
+    spot_2.phone_number = "(212) 777-7465"
+    spot_2.more_business_information = "Test More Business Info!"
 
-[spot_1,spot_2]
+    [spot_1,spot_2]
+  end
+
+
+  def self.scrape_yelp
+    doc = Nokogiri::HTML(open("http://www.yelp.com/search?attrs=NewBusiness&cflt=food&find_desc=&find_loc=New+York%2C+NY"))
+
+binding.pry
+
+
+
+  end
+
+
+
 end
 
-
-
-
-
-
-end
-
-
-
-
-#:name, :address,:location, :categories, :price_range, :hours, :more_business_information,  :phone_number
