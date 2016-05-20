30870452f3d451e74601b4e2ccb7fd97c7c8f69b
hangloos <ericloos00@gmail.com>
2016-05-20 14:52:06 -0400

    Ive gotten makeshift program working. Letting users enter numbers in to see more information and being able to exit.

--- lib/nyc_new_spots/cli.rb
+++ lib/nyc_new_spots/cli.rb
@@ -1,8 +1,58 @@
 class NycNewSpots::CLI
 
-
 def call
-puts "test"
+list_spots
+more_information
+goodbye
 end
 
+
+def list_spots
+  puts "New Places in New York:"
+  puts "1. Eric Test"
+  puts "2. Molly test"
+    #@spots = NycNewSpots::Spots.today
+    #@spots.each_with_index(1) do |spot, x|
+     # puts "#{x}. #{spot.name} - #{spot.location} - #{spot.price_range}"
+     # puts       "#{spot.categories}\n"
+      #puts       "Address: #{spot.address}"
+    #  puts       "Phone Number: #{spot.phone_number}"
+    #  puts       "Hours: \n"
+     # puts              "#{spot.hours}"
+     # end   
 end
+
+
+def more_information
+  input = nil
+  while input != "exit"
+    puts "Enter the number of the place you want to see more information on. Enter list to see the places again. Enter page 2 etc to see another page. Exit when you want to leave."
+  input = gets.strip.downcase  
+  if input == "1"
+    puts "1. Eric Test"
+  elsif input == "2"
+    puts "2. Molly test"
+  elsif input == "list"
+    puts "New Places in New York:"
+    puts "1. Eric Test"
+    puts "2. Molly test"
+  else
+      puts "List or exit"
+    end
+end
+
+
+
+
+  puts "More Business Information:\n"
+  #puts "#{spot.more_business_information}"
+end
+
+
+def goodbye
+  puts "Adios! Check back soon for the latest and greatest NYC has to offer!!"
+end
+
+
+end
+        
---
+++ notes.md
@@ -0,0 +1,11 @@
+#:name, :address,:location, :categories, :price_range, :hours, :more_business_information,  :phone_number
+
+#   1. Ludlow Coffe Supply - Lower East Side - Under $10
+#      Coffee & Tea, Breakfast & Brunch
+#      
+#      Address: 176 Ludlow St New York, NY 10002
+#      Phone Number: (212) 777-7465
+#      Hours:
+
+#            Mon  8:00am - 8:00pm
+#            Tue  8:00am - 8:00pm  
